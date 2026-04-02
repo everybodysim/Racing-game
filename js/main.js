fea947886cfab1ba456da57633ac1707eab8e957
everybodysimulator <ga1010@wscloud.org>
2026-04-02 09:07:10 -0400

    Update surface drivability

--- js/main.js
+++ js/main.js
@@ -73,8 +73,8 @@ const BOOST_FORCE_SECONDS = 0.45;
 const BOOST_ACCEL_PER_SECOND = 16.5;
 const VEHICLE_SURFACE_RADIUS = 0.5;
 const SURFACE_EFFECTS = {
-	'surface-wood': { grip: 1.55, drag: 1.35, accel: 1.0, drive: 1.55 },
-	'surface-ice': { grip: 0.7, drag: 0.58, accel: 0.45, drive: 1.0 },
+	'surface-wood': { grip: 0.9, drag: 1.35, accel: 1.0, drive: 1.55 },
+	'surface-ice': { grip: 0.4, drag: 0.58, accel: 0.45, drive: 0.8 },
 };
 
 function decodeExtrasParam( str ) {
